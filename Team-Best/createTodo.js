bce8df940b914f28cf294529f37914f8aba53787
unknown <flod96@hotmail.com>
2023-06-08 10:44:21 +0200

    Add more code

---
+++ Team-Best/createTodo.js
@@ -0,0 +1,8 @@
+const titleInput = document.getElementById('title-input');
+const descriptionInput = document.getElementById('description-input');
+const endDateInput = document.getElementById('end-date-input');
+const createTodo = document.getElementById('create-todo');
+
+function todoFunction() {
+
+} 
--- Team-Best/main.js
+++ Team-Best/main.js
@@ -1,2 +1,50 @@
 import './style.css'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAccE4Vn7dI2N-lBpK6UmoCWJBIxCLg8Gc",
+  authDomain: "team-best.firebaseapp.com",
+  projectId: "team-best",
+  storageBucket: "team-best.appspot.com",
+  messagingSenderId: "990147430148",
+  appId: "1:990147430148:web:7212d6957e86e22e51f251"
+};
+
+// Initialize Firebase
+function writePostData(userID, Title, Descriptiopn, Date) {
+const app = initializeApp(firebaseConfig);
+const db = getDatabase();
+const reference = ref(db, 'Todo/' + userID);
+
+set(reference, {
+  Title: name,
+  Descriptiopn: info,
+  Date: 2342
+});
+}
+
+writePostData("Jesper", "Test", "Mer test text", 1000);
+
+// firebase.initializeApp(firebaseConfig);
+
+//   var database = firebase.database();
+
+//   var ref = database.ref('users');
+
+
+// let database = app.database();
+// database = app.ref();
+// database.on('value', function(snapshot) {
+//   const data = snapshot.val();
+//   console.log(data);
+// });
+
+
+
+// createTodo();
