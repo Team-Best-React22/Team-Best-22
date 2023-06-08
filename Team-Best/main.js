009a63f9faa366559fc742fef6b7fa50783d7642
Phillipe <phillipe_jarnefors@hotmail.com>
2023-06-08 11:00:27 +0200

    Fixed firebase

--- Team-Best/main.js
+++ Team-Best/main.js
@@ -1,50 +1,29 @@
-import './style.css'
+import "./style.css"
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "firebase/app"
+import { getDatabase } from "firebase/database"
+import { getFirestore, collection, getDocs } from "firebase/firestore"
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAccE4Vn7dI2N-lBpK6UmoCWJBIxCLg8Gc",
   authDomain: "team-best.firebaseapp.com",
   projectId: "team-best",
   storageBucket: "team-best.appspot.com",
   messagingSenderId: "990147430148",
-  appId: "1:990147430148:web:7212d6957e86e22e51f251"
-};
-
-// Initialize Firebase
-function writePostData(userID, Title, Descriptiopn, Date) {
-const app = initializeApp(firebaseConfig);
-const db = getDatabase();
-const reference = ref(db, 'Todo/' + userID);
-
-set(reference, {
-  Title: name,
-  Descriptiopn: info,
-  Date: 2342
-});
+  appId: "1:990147430148:web:7212d6957e86e22e51f251",
 }
 
-writePostData("Jesper", "Test", "Mer test text", 1000);
+initializeApp(firebaseConfig)
 
-// firebase.initializeApp(firebaseConfig);
+const database = getFirestore()
 
-//   var database = firebase.database();
+const collectRef = collection(database, "todos")
 
-//   var ref = database.ref('users');
-
-
-// let database = app.database();
-// database = app.ref();
-// database.on('value', function(snapshot) {
-//   const data = snapshot.val();
-//   console.log(data);
-// });
-
-
-
-// createTodo();
+getDocs(collectRef).then((snapshot) => {
+  let todos = []
+  snapshot.docs.forEach((doc) => {
+    todos.push({ ...doc.data(), id: doc.id })
+  })
+  console.log(todos)
+})
